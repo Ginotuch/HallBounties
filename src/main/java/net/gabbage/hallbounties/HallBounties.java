591c1713b4c4b8334676a1d29ad3af26d7afffe4
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2020-04-17 17:14:28 +1200

    Removed unused variable

--- src/main/java/net/gabbage/hallbounties/HallBounties.java
+++ src/main/java/net/gabbage/hallbounties/HallBounties.java
@@ -1,6 +1,5 @@
 package net.gabbage.hallbounties;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HallBounties extends JavaPlugin {
@@ -8,7 +7,6 @@ public final class HallBounties extends JavaPlugin {
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
-        final FileConfiguration config = this.getConfig();
         this.getCommand("bounty").setExecutor(new Bounty(this));
 
     }
