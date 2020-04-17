0b778b01f9ea94bca35a43239562f28b2dfa39c7
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2020-04-17 16:12:37 +1200

    Moved code to methods, converted storage to hashmap

--- src/main/java/net/gabbage/hallbounties/Bounty.java
+++ src/main/java/net/gabbage/hallbounties/Bounty.java
@@ -5,20 +5,75 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Bounty implements CommandExecutor {
     HallBounties plugin;
     FileConfiguration config;
+    ConfigurationSection bounties;
 
     public Bounty(HallBounties hallBounties) {
         plugin = hallBounties;
         config = this.plugin.getConfig();
+        bounties = this.config.getConfigurationSection("bounties");
+    }
+
+    public void removeBounty(String key) {
+        Map<String, String> updated_bounties = new HashMap<>();
+
+        for (String bounty : this.bounties.getKeys(true)) {
+            if (!bounty.equals(key)) {
+                updated_bounties.put(bounty, this.bounties.getString(bounty));
+            }
+        }
+        this.config.createSection("bounties", updated_bounties);
+        this.plugin.saveConfig();
+        this.bounties = this.config.getConfigurationSection("bounties");
+    }
+
+    public void add(CommandSender sender, String bountyName, String bountyPrice) {
+        boolean isfloat;
+        Float price = null;
+        try {
+            price = Float.parseFloat(bountyPrice);
+            isfloat = true;
+        } catch (NumberFormatException e) {
+            isfloat = false;
+            sender.sendMessage(bountyPrice + " cannot be read as a number");
+        }
+        if (isfloat) {
+            Map<String, String> added_bounties = new HashMap<>();
+
+            for (String bounty : this.bounties.getKeys(true)) {
+                added_bounties.put(bounty, this.bounties.getString(bounty));
+            }
+
+            added_bounties.put(bountyName, price.toString());
+            this.config.createSection("bounties", added_bounties);
+            this.plugin.saveConfig();
+            this.bounties = this.config.getConfigurationSection("bounties");
+        }
+    }
+
+    public void pay(CommandSender sender, String playerName, String claimedBounty) {
+
+        Player payee = Bukkit.getServer().getPlayer(playerName);
+        if (payee == null) {
+            sender.sendMessage("Player \"" + playerName + "\" isn't online or doesn't exist");
+        } else if (!this.bounties.contains(claimedBounty)) {
+            sender.sendMessage("Bounty \"" + claimedBounty + "\" doesn't exist");
+        } else {
+            ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+            String consolecommand = "economy give " + playerName + " " + this.bounties.getString(claimedBounty);
+            Bukkit.dispatchCommand(console, consolecommand);
+
+            this.removeBounty(claimedBounty);
+        }
     }
 
     // This method is called, when somebody uses our command
@@ -28,52 +83,21 @@ public class Bounty implements CommandExecutor {
             if (args.length < 1) {
                 sender.sendMessage("Usages:\n /bounty list\n /bounty add <name> <price>\n /bounty pay <player_name>\n /bounty remove <bounty_name>");
             } else if (args[0].toLowerCase().equals("list")) {
-                List<String> bounties = this.config.getStringList("bounties");
                 sender.sendMessage("Bounties:");
-                for (String s : bounties) {
-                    if (this.config.contains(s)) {
-                        sender.sendMessage("  - " + s + ": " + config.getString(s));
-                    }
+                for (String s : this.bounties.getKeys(true)) {
+                    sender.sendMessage("  - " + s + ": " + this.bounties.getString(s));
                 }
             } else if (args[0].toLowerCase().equals("add")) {
                 if (args.length != 3) {
                     sender.sendMessage("Incorrect argument count.\nUsage: /bounty add <name> <price>");
                 } else {
-                    boolean isfloat;
-                    Float price = null;
-                    try {
-                        price = Float.parseFloat(args[2]);
-                        isfloat = true;
-                    } catch (NumberFormatException e) {
-                        isfloat = false;
-                        sender.sendMessage(args[2] + " cannot be read as a number.");
-                    }
-                    if (isfloat) {
-                        this.config.set(args[1], price.toString());
-                        List<String> temp_bounties = this.config.getStringList("bounties");
-                        if (!temp_bounties.contains(args[1])) {
-                            temp_bounties.add(args[1]);
-                        }
-                        this.config.set("bounties", temp_bounties);
-                        this.plugin.saveConfig();
-                    }
+                    this.add(sender, args[1], args[2]);
                 }
             } else if (args[0].toLowerCase().equals("pay")) {
                 if (args.length != 3) {
                     sender.sendMessage("Incorrect argument count.\nUsage: /bounty pay <name> <bounty_name>");
                 } else {
-                    Player payee = Bukkit.getServer().getPlayer(args[1]);
-                    List<String> temp_bounties = this.config.getStringList("bounties");
-                    if (payee == null) {
-                        sender.sendMessage("Player \"" + args[1] + "\" isn't online or doesn't exist");
-                    } else if (!temp_bounties.contains(args[2])) {
-                        sender.sendMessage("Bounty \"" + args[2] +"\" doesn't exist");
-                    }
-                    else {
-                        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-                        String consolecommand = "economy give " + args[1] + " " + this.config.getString(args[2]);
-                        Bukkit.dispatchCommand(console, consolecommand);
-                    }
+                    this.pay(sender, args[1], args[2]);
                 }
             }
 
